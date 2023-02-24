10c8702e9b7f0e94275085bc609883c12f6da758
Martina Patone <Martina.Patone@soglu.internal>
2023-02-24 17:36:36 +0100

    Remove R from man

--- man/R/zzz.R
+++
@@ -1,5 +0,0 @@
-#' @importFrom data.table fread fwrite
-#' @importFrom stats aggregate
-#' @importFrom utils menu read.csv tail write.csv txtProgressBar setTxtProgressBar
- 
-.onLoad<-function(libname, pkgname){}
