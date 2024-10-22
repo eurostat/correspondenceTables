cb0481a471bc3b284230dd6dfa9ce24699b3f600
bienvel <130038826+bienvel@users.noreply.github.com>
2024-10-22 14:55:31 +0200

    pull merge

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,5 @@
 #' @importFrom data.table fread fwrite
 #' @importFrom stats aggregate
 #' @importFrom utils menu read.csv tail write.csv txtProgressBar setTxtProgressBar
-#' @importFrom httr POST content accept
  
 .onLoad<-function(libname, pkgname){}
