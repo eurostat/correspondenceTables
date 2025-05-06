c0db38b308530cc32143f1b8657653fe5c4b5af1
bienvel <130038826+bienvel@users.noreply.github.com>
2025-05-06 15:15:51 +0200

    unit tests update

--- inst/tinytest/test_aggregateCorrespondenceTable.R
+++ inst/tinytest/test_aggregateCorrespondenceTable.R
@@ -1,7 +1,7 @@
-#TEST 1a
-AB = system.file("extdata", "ab_data.csv", package = "correspondenceTables")
-A = system.file("extdata", "a_data.csv", package = "correspondenceTables")
-B = system.file("extdata", "b_data.csv", package = "correspondenceTables")
+#TEST 1
+AB = system.file("extdata/test", "ab_data.csv", package = "correspondenceTables")
+A = system.file("extdata/test", "a_data.csv", package = "correspondenceTables")
+B = system.file("extdata/test", "b_data.csv", package = "correspondenceTables")
 
 ACT_normalT = aggregateCorrespondenceTable(AB, A, B, NULL)
 
@@ -10,19 +10,33 @@ ACText_normalT = utils::read.csv(ACText_normalT, sep = ",", header = TRUE, check
 
 expect_equal(ACT_normalT, ACText_normalT)
 
-#TEST 1b
-AB = system.file("extdata", "ab_data_duplicate.csv", package = "correspondenceTables")
-A = system.file("extdata", "a_data.csv", package = "correspondenceTables")
-B = system.file("extdata", "b_data.csv", package = "correspondenceTables")
+#TEST 2a
+AB = system.file("extdata/test", "ab_data_duplicate.csv", package = "correspondenceTables")
+A = system.file("extdata/test", "a_data.csv", package = "correspondenceTables")
+B = system.file("extdata/test", "b_data.csv", package = "correspondenceTables")
 
-expect_error(ACT_normalT= aggregateCorrespondenceTable(AB, A, B, NULL))
+expect_error(ACT_dupT= aggregateCorrespondenceTable(AB, A, B, NULL))
 
-#TEST 1c
-AB = system.file("extdata", "ab_data_empty.csv", package = "correspondenceTables")
-A = system.file("extdata", "a_data.csv", package = "correspondenceTables")
-B = system.file("extdata", "b_data.csv", package = "correspondenceTables")
+#TEST 2b
+AB = system.file("extdata/test", "ab_data_empty.csv", package = "correspondenceTables")
+A = system.file("extdata/test", "a_data.csv", package = "correspondenceTables")
+B = system.file("extdata/test", "b_data.csv", package = "correspondenceTables")
 
-expect_error(ACT_emptylT= aggregateCorrespondenceTable(AB, A, B, NULL))
+expect_error(ACT_emptyT= aggregateCorrespondenceTable(AB, A, B, NULL))
 
 
-#TEST 1d
+#TEST 2c
+
+AB = system.file("extdata/test", "ab_data_missingcode.csv", package = "correspondenceTables")
+A = system.file("extdata/test", "a_data.csv", package = "correspondenceTables")
+B = system.file("extdata/test", "b_data.csv", package = "correspondenceTables")
+
+expect_error(ACT_missingT= aggregateCorrespondenceTable(AB, A, B, NULL))
+
+#TEST 2d
+
+AB = system.file("extdata/test", "ab_data.csv", package = "correspondenceTables")
+A = system.file("extdata/test", "a_data_textsup.csv", package = "correspondenceTables")
+B = system.file("extdata/test", "b_data.csv", package = "correspondenceTables")
+
+expect_error(ACT_missingT= aggregateCorrespondenceTable(AB, A, B, NULL))
